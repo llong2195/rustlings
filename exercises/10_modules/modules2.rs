b9d60d4fa05fd68af7e6160fd0daacfc51d8238f
llong2195 <nduylong9501@gmail.com>
2026-02-02 18:07:24 +0700

    string, hashmap, module

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -3,15 +3,15 @@
 
 mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
-    // use self::fruits::PEAR as ???;
-    // use self::veggies::CUCUMBER as ???;
+    pub use self::fruits::PEAR as fruits;
+    pub use self::veggies::CUCUMBER as veggies;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
@@ -20,7 +20,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
+        delicious_snacks::fruits,
+        delicious_snacks::veggies,
     );
 }
